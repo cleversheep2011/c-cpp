d7c85d57115c35bab50d52e4c5ce01c02e8c1b8c
cleversheep2011 <cleversheep2011@outlook.com>
2023-02-23 11:09:33 +0800

    commit

--- 202302/d20230222array.cpp
+++ 202302/d20230222array.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-	int a[5] = {1,2,3,4,5};  // 定义一个有五个项的数组
-	std::cout << a[0] << std::endl;  // 获取列表第一项(c++语言数组索引从0开始 所以第一项就是0)
-	for (int i=0;i<5;i++)  // 遍历数组
+	int a[5] = {1,2,3,4,5};
+	std::cout << a[0] << std::endl;
+	for (int i=0;i<5;i++)
 	{
 		std::cout << a[i] << std::endl;
 	}
-	return 0;  // 程序结束
+	return 0;
 }
