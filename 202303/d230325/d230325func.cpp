f6a3777fc336056a3f4f57d5e47291eb83bbe9eb
某sheep建议张轩晨别用--global了 <12473581+zhangxuanzi@user.noreply.gitee.com>
2023-03-25 18:09:34 +0800

    commit

---
+++ 202303/d230325/d230325func.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hlgs.h"
+
+int main()
+{
+	std::cout << hlgs(1, 1, 1, 4, 5, 1) << std::endl;
+	return 0;
+}
---
+++ 202303/d230325/hlgs.h
@@ -0,0 +1,1 @@
+double hlgs(int, int, int, int, int, int);
