b730ff642ba75103d0baa907071b6a946da1efcf
某sheep建议张轩晨别用--global了 <12473581+zhangxuanzi@user.noreply.gitee.com>
2023-03-25 18:34:12 +0800

    commit

--- 202303/d230325/d230325func.cpp
+++ 202303/d230325/d230325func.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "hlgs.h"
-
+#define sheep "beebeeimsheep"
 int main()
 {
+	std::cout << sheep << std::endl;
 	std::cout << hlgs(1, 1, 1, 4, 5, 1) << std::endl;
 	return 0;
 }
--- 202303/d230325/hlgs.cpp
+++ 202303/d230325/hlgs.cpp
@@ -1,5 +1,5 @@
 #include <cmath>
-
+// bee bee im sheep
 double hlgs(int x1, int y1, int x2, int y2, int x3, int y3)
 {
 	double p1 = sqrt(pow(abs(x1 - x2), 2)+pow(abs(y1 - y2), 2));
